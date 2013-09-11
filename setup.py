fb7c72e129b3e91492c5b5f186c7769a5d38f355
Sebastian Böck <sebastian.boeck@jku.at>
2013-09-11 18:44:36 +0200

    make ready to use with distutils, simply run python setup.py sdist

--- setup.py
+++ setup.py
@@ -1,14 +1,23 @@
-from setuptools import setup
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+This file contains the setup for setuptools to distribute as a package.
+
+@author: Sebastian Böck <sebastian.boeck@jku.at>
+
+"""
+
+from distutils.core import setup
 
 setup(name='cpjku',
       version='0.01',
       description='Python package used at cp.jku.at',
       long_description=open('README').read(),
-      #url='http://github.com/CPJKU/python_cpjku',
-      author='Department of Computational Perception, Johannes Kepler University, Linz, Austria',
-      author_email='python_cpjku@jku.at',
-      license='BSD',
-      packages=['cp'],
+      author='Sebastian Böck, Department of Computational Perception, Johannes Kepler University, Linz, Austria',
+      author_email='sebastian.boeck@jku.at',
+      url='http://www.cp.jku.at',
+      license='BSD, with some exclusions',
+      include_package_data=True,
       install_requires=[
           'numpy',
           'scipy'
