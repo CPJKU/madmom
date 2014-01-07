61ebf4c35be621d515ccab01fda68b040bf0dd2f
Sebastian Böck <sebastian.boeck@jku.at>
2014-01-07 16:44:09 +0100

    added encoding information and missing docs

--- madmom/__init__.py
+++ madmom/__init__.py
@@ -1,3 +1,18 @@
+# encoding: utf-8
+"""
+This package is used internally by the Department of Computational Perception,
+Johannes Kepler University, Linz, Austria (http://www.cp.jku.at) and the
+Austrian Research Institute for Artificial Intelligence (OFAI), Vienna, Austria
+(http://www.ofai.at).
+
+To install the package, simply run:
+
+python setup.py install
+
+You might need higher privileges (use su or sudo) to install the files to a
+common place like /usr/local or similar.
+
+"""
 import audio
 import features
 import evaluation
--- madmom/audio/__init__.py
+++ madmom/audio/__init__.py
@@ -1,3 +1,10 @@
+# encoding: utf-8
+"""
+This package includes audio handling functionality and lower level features.
+The definition of "lower" may vary, but all "higher" level features (e.g. beats,
+onsets, etc.) can be found in the `features` package.
+
+"""
 import signal
 import wav
 import spectrogram
