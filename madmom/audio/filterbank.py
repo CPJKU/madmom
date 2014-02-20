05d7484db6d484565b49e0ab595f45ccd11902d3
Sebastian Böck <sebastian.boeck@jku.at>
2014-02-20 11:23:55 +0100

    clarified some variable names

--- madmom/audio/filterbank.py
+++ madmom/audio/filterbank.py
@@ -318,19 +318,19 @@ def _put_filter(filt, band):
     """
     start = filt.start_pos
     stop = start + len(filt.filter)
-    fltr = filt.filter
+    filter_ = filt.filter
     # truncate the filter if it starts before the 0th frequency bin
     if start < 0:
-        fltr = fltr[-start:]
+        filter_ = filter_[-start:]
         start = 0
     # truncate the filter if it ends after the last frequency bin
     if stop > len(band):
-        fltr = fltr[:stop - len(band)]
+        filter_ = filter_[:stop - len(band)]
         stop = len(band)
     # put the filter in place
     filt_pos = band[start:stop]
     # TODO: if needed allow other handling (like adding values)
-    np.maximum(fltr, filt_pos, out=filt_pos)
+    np.maximum(filter_, filt_pos, out=filt_pos)
 
 
 def assemble_filterbank(filters, num_fft_bins, norm):
@@ -425,16 +425,15 @@ def filterbank(filter_type, frequencies, num_fft_bins, sample_rate,
     """
     # map the frequencies to the spectrogram bins
     factor = (sample_rate / 2.0) / num_fft_bins
-    frequencies = np.round(np.asarray(frequencies) / factor).astype(int)
-    # filter out all frequencies outside the valid range
-    frequencies = frequencies[:np.searchsorted(frequencies, num_fft_bins)]
+    bins = np.round(np.asarray(frequencies) / factor).astype(int)
+    # filter out all bins outside the valid range
+    bins = bins[:np.searchsorted(bins, num_fft_bins)]
     # FIXME: skip the DC bin 0?
 
     # create filter bank
     filters = []
-    # get (overlapping) start, center and stop frequencies from a list of
-    # center frequencies
-    for start, center, stop in band_bins(frequencies, duplicates, overlap):
+    # get (overlapping) start, center and stop frequencies from a list of bins
+    for start, center, stop in band_bins(bins, duplicates, overlap):
         # set filter arguments
         kwargs = {'width': stop - start,
                   'center': center - start,
