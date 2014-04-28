e5b8d462117b60b4419d7566c592d88a568abd37
Sebastian Böck <sebastian.boeck@jku.at>
2014-04-28 13:05:40 +0200

    added license file for the trained neural networks

--- bin/PianoTranscriptor.py
+++ bin/PianoTranscriptor.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # encoding: utf-8
 """
-Copyright (c) 2012-2013 Sebastian Böck <sebastian.boeck@jku.at>
+Copyright (c) Sebastian Böck <sebastian.boeck@jku.at>
 
 Redistribution in any form is not permitted!
 
