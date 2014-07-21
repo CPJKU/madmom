2757ec0d860ebbe64f59789a4662d56cfa3339ea
Sebastian Böck <sebastian.boeck@jku.at>
2014-07-21 15:57:34 +0200

    added comments to crf_viterbi

--- madmom/features/crf_viterbi.pyx
+++ madmom/features/crf_viterbi.pyx
@@ -34,28 +34,38 @@ def viterbi(np.ndarray[np.float32_t, ndim=1] pi,
                         and log probability of beat sequence
 
     """
+    # number of states
     cdef int num_st = activations.shape[0]
+    # number of transitions
     cdef int num_tr = transition.shape[0]
+    # number of beat variables
     cdef int num_x = num_st / tau
 
+    # back-tracking pointer sequence
     cdef list bps = []
+    # current viterbi variables
     cdef np.ndarray[np.float32_t, ndim=1] v_c = np.zeros(num_st, dtype=np.float32)
+    # previous viterbi variables
     cdef np.ndarray[np.float32_t, ndim=1] v_p = np.zeros(num_st, dtype=np.float32)
-    cdef np.ndarray[np.int_t, ndim=1] bp_c = np.ones_like(v_c, dtype=int)
+    # current back-tracking pointers; init them with -1s
+    cdef np.ndarray[np.int_t, ndim=1] bp_c = np.ones_like(v_c, dtype=int) * -1
+    # back tracked path, a.k.a. path sequence
     cdef list path = []
 
+    # counters etc.
     cdef int k, i, j
     cdef double cur, new, sum_k, log_sum = 0.0
 
-    bp_c[:] = -1
-
+    # init first beat
     v_p = pi * activations
     v_p /= v_p.sum()
 
+    # iterate over all beats; the 1st beat is given by prior
     for k in range(num_x - 1):
+        # reset all current viterbi variables
         for i in range(num_st):
             v_c[i] = 0.0
-
+        # search the best transition
         for i in range(num_st):
             for j in range(num_tr):
                 if (i + j) >= num_st:
@@ -80,10 +90,12 @@ def viterbi(np.ndarray[np.float32_t, ndim=1] pi,
         v_p, v_c = v_c, v_p
         bps.append(bp_c.copy())
 
+    # add the final best state to the path
     next_state = v_p.argmax()
     path.append(next_state)
+    # track the path backwards
     for i in range(num_x - 2, -1, -1):
         next_state = bps[i][next_state]
         path.append(next_state)
-
+    # return the best sequence and its log probability
     return np.array(path[::-1]), log(v_p.max()) + log_sum
