528f331d2a78063a4683f28b69cfe04e11e451fb
Sebastian Böck <sebastian.boeck@jku.at>
2016-07-28 13:45:47 +0200

    bump version to 0.15.dev0

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ import glob
 import numpy as np
 
 # define version
-version = '0.14'
+version = '0.15.dev0'
 
 # define which extensions need to be compiled
 extensions = [Extension('madmom.audio.comb_filters',
