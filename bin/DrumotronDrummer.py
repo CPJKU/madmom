8e7902a67f92c413ecde58f477078c2a0842788c
Florian <florian.krebs@jku.at>
2017-01-07 19:26:31 +0100

    drum control

---
+++ bin/DrumotronDrummer.py
@@ -0,0 +1,130 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+Drumotron beat tracking, pattern selection and drum control algorithm.
+
+Drumotron uses different algorithms for different parts:
+1. DBNBeatTracker for tracking the beats,
+2. GMMBarTracker for tracking the bar and selecting the pattern,
+3. logic to control the servos to play a drum kit.
+
+The logic of 1. and 2. is incorporated and modified within this file, since it
+needs to run on a Raspberry Pi. However, to be able to do so, the different
+parts need to run on different CPU cores, since the Pi has rather limited
+processing power.
+
+"""
+
+from __future__ import absolute_import, division, print_function
+
+import argparse
+import multiprocessing as mp
+from functools import partial
+
+import numpy as np
+
+from madmom.processors import (IOProcessor, io_arguments, SequentialProcessor,
+                               ParallelProcessor)
+from madmom.audio.signal import SignalProcessor, FramedSignalProcessor
+from madmom.audio.stft import ShortTimeFourierTransformProcessor
+from madmom.audio.spectrogram import (FilteredSpectrogramProcessor,
+                                      LogarithmicSpectrogramProcessor,
+                                      SpectrogramDifferenceProcessor)
+from madmom.ml.nn import NeuralNetwork, NeuralNetworkEnsemble
+from madmom.models import BEATS_LSTM, PATTERNS_GUITAR, DRUM_PATTERNS
+from madmom.features.beats import DBNBeatTrackingProcessor
+from madmom.features.downbeats import BeatSyncProcessor, GMMBarProcessor
+from madmom.drumotron import DrumotronControlProcessor
+from madmom.drumotron import DrumotronHardwareProcessor
+
+
+def main():
+    """Drumotron"""
+
+    # define parser
+    p = argparse.ArgumentParser(
+        formatter_class=argparse.RawDescriptionHelpFormatter, description='''
+    DrumotronDrummer
+    ''')
+    # version
+    p.add_argument('--version', action='version',
+                   version='Drumotron.2016')
+    # input/output options
+    io_arguments(p, output_suffix=None, online=True)
+    # signal processing arguments
+    SignalProcessor.add_arguments(p, norm=False, gain=0)
+    # tracking arguments
+    DBNBeatTrackingProcessor.add_arguments(p, min_bpm=70, max_bpm=150)
+    GMMBarProcessor.add_arguments(p)
+
+    # parse arguments
+    args = p.parse_args()
+
+    # set immutable arguments
+    args.fps = 100
+
+    # print arguments
+    if args.verbose:
+        print(args)
+
+    # define signal processing used by all others
+    sig = SignalProcessor(num_channels=1, sample_rate=44100)
+    frames = FramedSignalProcessor(frame_size=2048, **vars(args))
+    stft = ShortTimeFourierTransformProcessor()  # caching FFT window
+    sig_proc = SequentialProcessor((sig, frames, stft))
+
+    # here, the processing for the RNN & GMM diverges
+
+    # beat tracking processor
+    filt = FilteredSpectrogramProcessor(num_bands=12, fmin=30,
+                                        fmax=17000, norm_filters=True)
+    spec = LogarithmicSpectrogramProcessor(mul=1, add=1)
+    diff = SpectrogramDifferenceProcessor(diff_ratio=0.5, positive_diffs=True,
+                                          stack_diffs=np.hstack)
+    # nn = NeuralNetworkEnsemble.load(BEATS_LSTM)
+    nn = NeuralNetwork.load(BEATS_LSTM[3])
+    dbn = DBNBeatTrackingProcessor(**vars(args))
+    beat_processor = SequentialProcessor((filt, spec, diff, nn, dbn))
+
+    # gmm feature
+    filt = FilteredSpectrogramProcessor(num_bands=12, fmin=60,
+                                        fmax=17000, norm_filters=True)
+    spec = LogarithmicSpectrogramProcessor(mul=1, add=1)
+    diff = SpectrogramDifferenceProcessor(diff_ratio=0.5, positive_diffs=True)
+    agg = partial(np.sum, axis=1)
+    gmm_feat_processor = SequentialProcessor((filt, spec, diff, agg))
+
+    # extract beat & gmm feature in parallel
+    beat_downbeat_processor = ParallelProcessor((beat_processor,
+                                                 gmm_feat_processor))
+
+    # sync the features to the beats
+    beat_sync = BeatSyncProcessor(**vars(args))
+    # score them with a GMM
+    gmm_bar_processor = GMMBarProcessor(pattern_files=PATTERNS_GUITAR,
+                                        pattern_change_prob=0.001,
+                                        **vars(args))
+    dhp = DrumotronHardwareProcessor()
+    dp = DrumotronControlProcessor(fps, DRUM_PATTERNS, delay=0, out=dhp)
+
+    # output handler
+    if args.online:
+        # simply output the given string
+        from madmom.utils import write_output as writer
+    elif args.downbeats:
+        # simply write the timestamps of the downbeats
+        from madmom.utils import write_events as writer
+    else:
+        # borrow the note writer for outputting timestamps + beat numbers
+        from madmom.features.notes import write_notes as writer
+
+    # create an IOProcessor
+    processor = IOProcessor([sig_proc, beat_downbeat_processor, beat_sync,
+                             gmm_bar_processor], writer)
+
+    # and call the processing function
+    args.func(processor, **vars(args))
+
+
+if __name__ == '__main__':
+    main()
---
+++ madmom/drumotron.py
@@ -0,0 +1,120 @@
+# encoding: utf-8
+"""
+This file contains functions to control drumotron, a drum robot.
+
+"""
+
+from __future__ import absolute_import, division, print_function
+import numpy as np
+import pickle
+from madmom.processors import Processor
+
+
+class DrumotronHardwareProcessor(Processor):
+
+    def __init__(self, arduino=False):
+        import serial
+        self.ser = serial.Serial('/dev/ttyACM0', 9600)
+        self.arduino = arduino
+
+    def process(self, cmd):
+        if self.arduino:
+            self.ser.write(cmd)
+        print('command ', cmd)
+
+
+class DrumotronControlProcessor(Processor):
+    """
+    Plays drums.
+
+    Parameters
+    ----------
+    grid : int, optional
+        Number of grid points per beat.
+    """
+    def __init__(self, fps, pattern_files, delay=0, smooth_win_len=0,
+                 grid=4, out=print):
+        # store parameters
+        self.fps = fps
+        self.delay = delay
+        self.smooth_win_len = smooth_win_len
+        self.grid = grid
+        # load patterns
+        self.patterns = [dict(np.load(pf)) for pf in pattern_files]
+        # apply discretisation
+        for p in range(len(self.patterns)):
+            self.patterns[p]['hh'] = [int(np.round(float(h) * float(grid)))
+                                      for h in self.patterns[p]['hh']]
+            self.patterns[p]['sn'] = [int(np.round(float(h) * float(grid)))
+                                      for h in self.patterns[p]['sn']]
+            self.patterns[p]['bd'] = [int(np.round(float(h) * float(grid)))
+                                      for h in self.patterns[p]['bd']]
+        self.num_beats = [int(p['num_beats']) for p in self.patterns]
+        # variables for storing intermediate data
+        self.last_position = None
+        self.beat_frame_counter_int = None
+        # self.beat_periods = [None] * smooth_win_len
+        self.current_beat_period = None
+        self.beat_count = None
+        self.pattern_id = None
+        self.beat_grid = None
+        self.beat_frame_counter_ext = None
+        self.last_played_position = None
+        self.out = out
+
+    def process(self, data):
+        """
+        Play drums
+
+        Parameters
+        ----------
+        data : tuple (beat_interval, beat_count, pattern_id)
+
+        Returns
+        -------
+        hit : None or numpy array
+            Defines if and which drum to hit
+
+        """
+        # print('======= %s ======' % str(self.beat_frame_counter_int))
+        (beat_interval, beat_count, pattern_id) = data
+        is_beat = beat_count is not None
+        if is_beat:
+            # print('--- beat ---')
+            self.pattern_id = pattern_id
+            self.current_beat_period = beat_interval
+            self.beat_count = beat_count
+            self.beat_frame_counter_int = self.delay
+            self.beat_frame_counter_ext = self.beat_frame_counter_int
+            # create bins to relate the frame counter to the beat grid
+            self.beat_grid = np.linspace(0, beat_interval, self.grid + 1)[:-1]
+        if self.beat_frame_counter_int is None:
+            return None
+        if (is_beat is False) and (self.beat_frame_counter_int >
+                                   self.current_beat_period - 1):
+            # print('-- new int beat')
+            # start new bar
+            self.beat_frame_counter_int = 0
+            # increase beat counter
+            self.beat_count = self.beat_count % self.patterns[
+                self.pattern_id]['num_beats'] + 1
+        current_position = np.digitize(
+            self.beat_frame_counter_int, self.beat_grid) - 1 + \
+            (self.beat_count - 1) * self.grid
+
+        if self.last_position != current_position:
+            self.last_position = current_position
+            if current_position != self.last_played_position:
+                if current_position in self.patterns[self.pattern_id]['hh']:
+                    self.out('1')
+                    self.last_played_position = current_position
+                if current_position in self.patterns[self.pattern_id]['sn']:
+                    self.out('2')
+                    self.last_played_position = current_position
+                if current_position in self.patterns[self.pattern_id]['bd']:
+                    self.out('3')
+                    self.last_played_position = current_position
+        # update state variables
+        self.beat_frame_counter_int += 1
+        self.beat_frame_counter_ext += 1
+        self.last_position = current_position
